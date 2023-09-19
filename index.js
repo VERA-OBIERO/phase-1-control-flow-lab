54b277387327992dfb2ece23b9342929b7b88457
VERA-OBIERO <vera.obiero@gmail.com>
2023-09-19 12:16:42 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  // Write your code here! 
+  if (feet <= 400){
+    return "This one is on me!"
+  }
+  if (feet > 2000 && feet < 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else
+    return "No can do."
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+   if (city === "NYC"){
+    return "Ok, sounds good."
+  } else
+      return "No go."
+}
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  if (tip === "generous"){
+    return "Thank you so much."
+  }
+  if (tip === "not as generous"){
+    return "Thank you."
+  }
+  else
+  return "Bye."
 }
